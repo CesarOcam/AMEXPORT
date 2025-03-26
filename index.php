d2dce740baa0313a22294a165a86b43b339fb42b
CesarOcam <cesar_po21@hotmail.es>
2025-03-26 12:37:01 -0600

    Primer commit del proyecto

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Estilos -->
+    <link rel="stylesheet" href="lib/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+
+    <title>Portal AMEXPORT</title>
+</head>
+<body>
+    <!-- Barra de navegación oscura -->
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
+        <div class="container">
+            <a class="navbar-brand" href="#">PORTAL AMEXPORT</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="https://ejemplo.com" aria-label="Search">
+                <button class="btn btn-secondary" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <div class="container-fluid pt-5">
+        <div class="m-2">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                       <!--CARDS-->
+                        <div class="row">
+                            <!-- Seccion 1 -->
+                            <div class="col-6">
+                                <!-- Card -->
+                                <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3 " style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                                <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                                                                 <!-- Card -->
+                                <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3 " style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                                <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                            </div>
+
+                            <!-- Seccion 2 -->
+                            <div class="col-6">
+                                <!-- Card -->
+                                <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                                 <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                                                                 <!-- Card -->
+                                <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3 " style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                                <a href="" style="text-decoration: none;">
+                                    <div class="card mb-3" style="max-width: 540px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 d-flex justify-content-center align-items-center icon-custom">
+                                                <!-- Ícono en lugar de la imagen -->
+                                                <i class="fas fa-globe fa-5x"></i><!-- Puedes cambiar "fa-heart" por el ícono que desees -->
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">URL</h5>
+                                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                 </a>
+                                <!-- Card -->
+                            </div>
+                        </div>
+                        <!--CARDS-->
+                    </div>
+
+                    <!--CONTENIDO-->
+                    <div class="col-12 col-md-6">
+                        <div class="container-fluid">
+                            <div class="card2 border rounded p-2 mb-3" style="width: 100%;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Información importante</h5>
+                                    <p class="card-text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eveniet autem nemo minus, consectetur commodi inventore neque non amet perspiciatis dolor dolore impedit harum, nihil dolores ducimus itaque necessitatibus soluta blanditiis optio veritatis deserunt odio, officiis obcaecati? Corrupti alias aspernatur nulla nobis exercitationem non accusamus omnis! Quo, reprehenderit facere. Incidunt, a vitae. Voluptas similique eos quae vel quod iste voluptates quibusdam iure minima aliquam, incidunt impedit consequatur ea esse enim quis quo, excepturi veniam illum minus porro. Cupiditate recusandae sunt, sapiente aspernatur nemo molestias nostrum, aliquid nihil, distinctio ad blanditiis. Aut recusandae accusamus odit adipisci, eius doloribus. Atque expedita a iure laboriosam! Pariatur necessitatibus voluptatum laudantium nisi eos reprehenderit tempora officiis labore, adipisci incidunt laboriosam voluptates natus veniam fugit placeat distinctio cupiditate voluptatem suscipit quod explicabo aut magnam. Dignissimos id eligendi cupiditate et corrupti dolores sit dolore aut, atque perferendis tempore, nesciunt blanditiis sint ut quaerat nemo rem pariatur iste!
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="p-3 border rounded">
+                                <table class="table table-striped table-hover">
+                                    <thead class="text-center">
+                                        <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">URL</th>
+                                        <th scope="col">Acción</th>
+                                        </tr>
+                                        <tbody>
+                                            <tr>
+                                            <th scope="row">1</th>
+                                            <td>Figma</td>
+                                            <td>https://www.figma.com/design/hgwgL5P8Y7k7RiJOvQjrN5/Untitled?node-id=0-1&p=f&t=H70CPbeXaB4RCOfm-0</td>
+                                            <td><a href="">Visitar</a></td>
+                                            </tr>
+                                            <tr>
+                                            <th scope="row">2</th>
+                                            <td>AMEXPORT</td>
+                                            <td>https://grupoamexport.com/</td>
+                                            <td><a href="">Visitar</a></td>
+                                            </tr>
+                                            <tr>
+                                            <th scope="row">3</th>
+                                            <td>AWS</td>
+                                            <td>https://aws.amazon.com/es/</td>
+                                            <td><a href="">Visitar</a></td>
+                                            </tr>
+
+                                        </tbody>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!--CONTENIDO-->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Script -->
+    <script src="lib/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
